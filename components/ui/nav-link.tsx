7ce799757fcc729a55b2a461a0ac00ec0b1715a5
joshuamwo <joshuamuuo5@gmail.com>
2023-10-31 19:48:16 +0300

    side bar nav links

---
+++ components/ui/nav-link.tsx
@@ -0,0 +1,41 @@
+import ActiveLink from "./active-link";
+import { classnames } from "@/utils/classnames";
+
+interface NavLinkProps {
+  href: string;
+  title: string;
+  icon: React.ReactNode;
+  sidebarIsOpen?: boolean;
+}
+
+export default function NavLink({
+  href,
+  icon,
+  title,
+  sidebarIsOpen,
+}: NavLinkProps) {
+  return (
+    <ActiveLink
+      href={href}
+      className="my-0.5 flex items-center gap-1 px-4 py-3 hover:bg-light-300 hover:dark:bg-dark-300 xs:px-6 sm:my-1 sm:gap-1.5 sm:px-7 lg:gap-2 xl:my-0.5"
+      activeClassName="text-dark-100 active-text-dark dark:active-text-light dark:text-light-400 font-medium bg-light-400 dark:bg-dark-400 hover:bg-light-600 hover:dark:bg-dark-500"
+    >
+      <span
+        className={classnames(
+          "flex flex-shrink-0 items-center justify-start",
+          sidebarIsOpen ? "w-auto xl:w-8" : "w-8 xl:w-auto"
+        )}
+      >
+        {icon}
+      </span>
+      <span
+        className={classnames(
+          "text-dark-100 dark:text-light-400",
+          sidebarIsOpen ? "hidden xl:inline-flex" : "inline-flex xl:hidden"
+        )}
+      >
+        {title}
+      </span>
+    </ActiveLink>
+  );
+}
