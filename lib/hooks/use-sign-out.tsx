bb9113fc094bf595a599ff1de5e40f3706d695f7
joshuamwo <joshuamuuo5@gmail.com>
2023-11-18 19:28:30 +0300

    commented out the useSignOut hook
    will work on it when i decide i need a hook for dign out

--- lib/hooks/use-sign-out.tsx
+++ lib/hooks/use-sign-out.tsx
@@ -1,40 +1,40 @@
-import { useSupabase } from "@/context/supabase-context";
-import { useRecoilState } from "@/recoil/recoil";
-import { userState } from "@/recoil/atoms";
-import { useRouter } from "next/navigation";
-import { SupabaseClient } from "@supabase/supabase-js";
+// import { useSupabase } from "@/context/supabase-context";
+// import { useRecoilState } from "@/recoil/recoil";
+// import { userState } from "@/recoil/atoms";
+// import { useRouter } from "next/navigation";
+// import { SupabaseClient } from "@supabase/supabase-js";
 
-interface signOutProps {
-  loading: boolean;
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
-  supabase: SupabaseClient<any, "public", any>;
-}
+// interface signOutProps {
+//   loading: boolean;
+//   setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+//   supabase: SupabaseClient<any, "public", any>;
+// }
 
-const signOut = async ({ loading, setLoading, supabase }: signOutProps) => {
-  setLoading(true);
-  const [user, setUser] = useRecoilState(userState);
-  const router = useRouter();
-  // Check if we have a session
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+// const signOut = async ({ loading, setLoading, supabase }: signOutProps) => {
+//   setLoading(true);
+//   const [user, setUser] = useRecoilState(userState);
+//   const router = useRouter();
+//   // Check if we have a session
+//   const {
+//     data: { session },
+//   } = await supabase.auth.getSession();
 
-  if (session) {
-    await supabase.auth
-      .signOut()
-      .then((res) => {
-        setUser(null);
-        router.refresh();
-        setLoading(false);
-      })
-      .catch((err) => {
-        router.refresh();
-        setLoading(false);
-      });
-  }
-  router.refresh();
-};
+//   if (session) {
+//     await supabase.auth
+//       .signOut()
+//       .then((res) => {
+//         setUser(null);
+//         router.refresh();
+//         setLoading(false);
+//       })
+//       .catch((err) => {
+//         router.refresh();
+//         setLoading(false);
+//       });
+//   }
+//   router.refresh();
+// };
 
-export function useSignOut({ loading, setLoading, supabase }: signOutProps) {
-  return signOut({ loading, setLoading, supabase });
-}
+// export function useSignOut({ loading, setLoading, supabase }: signOutProps) {
+//   return signOut({ loading, setLoading, supabase });
+// }
