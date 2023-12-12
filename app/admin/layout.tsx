994ea1c2babd3c5dd8718375ccf54f6f6e3820d8
joshuamwo <joshuamuuo5@gmail.com>
2023-12-12 19:13:50 +0300

    user check in /admin will only happen once getUser is done

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -7,22 +7,25 @@ import { UserStateType } from "@/types";
 import { useRecoilValue } from "recoil";
 import { userState } from "@/recoil/atoms";
 import { useIsMounted } from "@/lib/hooks/use-is-mounted";
+import { getUserDoneState } from "@/recoil/atoms";
+import { get } from "http";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   const user = useRecoilValue<UserStateType | null>(userState);
   const { openModal } = useModalAction();
   const router = useRouter();
   const isMounted = useIsMounted();
-
+  const getUserDone = useRecoilValue(getUserDoneState);
 
   useEffect(() => {
+    if (!getUserDone) return;
     if (!user) {
       router.push("/");
       openModal("AUTHFORM");
     } else if (user.user_type !== "alpha") {
       router.push("/");
     }
-  });
+  }, [getUserDone]);
 
   return <>{user && user.user_type == "alpha" && children}</>;
 }
