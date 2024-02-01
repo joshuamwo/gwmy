3d9b41c2d3afd0717b0c9dd5075933e8a89e8922
yulluone <joshuamuuo5@gmail.com>
2024-02-01 18:55:00 +0300

    user auth abstracted

---
+++ app/actions/authorise-user.ts
@@ -0,0 +1,28 @@
+import { useSupabase } from "./supabase-server";
+
+export async function ValidateUser() {
+  const supabase = useSupabase();
+  const { data, error } = await supabase.from("profiles").select("*");
+
+  function isAdmin(): boolean {
+    //handle error and user unauthorised
+    if (error) {
+      return false;
+    } else if (data[0].user_type === "alpha") {
+      return true;
+    }
+    return false;
+  }
+
+  function isAuthenticated(): boolean {
+    //handle error and user unauthorised
+    if (error) {
+      return false;
+    } else if (!data || data.length < 1) {
+      return false;
+    }
+    return true;
+  }
+
+  return { isAdmin, isAuthenticated };
+}
