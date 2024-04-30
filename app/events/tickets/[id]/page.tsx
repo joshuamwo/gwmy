ad56d9b3939698eec9ddacb41cedab82332a6e6e
yuluone <joshuamuuo5@gmail.com>
2024-04-30 11:37:03 +0300

    ticket looking nice

--- app/events/tickets/[id]/page.tsx
+++ app/events/tickets/[id]/page.tsx
@@ -5,6 +5,8 @@ import "./styles.css";
 import { useSupabase } from "@/context/supabase-context";
 import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { Event } from "@/types";
+import Image from "next/image";
 
 interface Ticket {
   holders_name: string;
@@ -15,6 +17,68 @@ export default function Ticket({ params }: { params: { id: string } }) {
   const ticketId = params.id;
 
   const [ticket, setTicket] = useState<Ticket>();
+  const [event, setEvent] = useState<Event>();
+  const [startDate, setStartDate] = useState<{
+    day: number;
+    month: string;
+    dayOfWeek: string;
+    year: number;
+  }>();
+  const [endDate, setEndDate] = useState<{
+    day: number;
+    month: string;
+    dayOfWeek: string;
+    year: number;
+  }>();
+
+  const [startTime, setStartTime] = useState<string>();
+  const [endTime, setEndTime] = useState<string>();
+
+  function formatDate(dateString: string) {
+    const months = [
+      "JAN",
+      "FEB",
+      "MAR",
+      "APR",
+      "MAY",
+      "JUN",
+      "JUL",
+      "AUG",
+      "SEP",
+      "OCT",
+      "NOV",
+      "DEC",
+    ];
+    const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const dayOfWeek = daysOfWeek[date.getDay()];
+    const year = date.getFullYear();
+
+    const formattedDate = {
+      day,
+      month,
+      dayOfWeek,
+      year,
+    };
+    console.log(formattedDate);
+    return formattedDate;
+  }
+
+  function formatTime(timeString: string) {
+    const [hours, minutes] = timeString.split(":");
+    let formattedTime;
+    if (Number(hours) < 12) {
+      formattedTime = `${parseInt(hours)}:${minutes} AM`;
+    } else if (hours === "12") {
+      formattedTime = `12:${minutes} PM`;
+    } else {
+      formattedTime = `${parseInt(hours) - 12}:${minutes} PM`;
+    }
+    return formattedTime;
+  }
 
   //fetch ticket & event from supabase
   const { supabase } = useSupabase();
@@ -28,10 +92,29 @@ export default function Ticket({ params }: { params: { id: string } }) {
       if (error) throw error;
       console.log(data);
       setTicket(data[0]);
+      await fetchEvent(data[0].event_id);
     } catch (error) {
       console.error("Error fetching ticket", error);
     }
   }
+
+  const fetchEvent = async (eventId: string) => {
+    try {
+      const { error, data } = await supabase
+        .from("events")
+        .select("*")
+        .eq("id", eventId);
+
+      if (error) throw error;
+      setEvent(data[0]);
+      setStartDate(formatDate(data[0].start_date));
+      setEndDate(formatDate(data[0].end_date));
+      setStartTime(formatTime(data[0].start_time));
+      setEndTime(formatTime(data[0].end_time));
+    } catch (error) {
+      // console.error("Error fetching event", error);
+    }
+  };
 
   useEffect(() => {
     fetchTicket();
@@ -43,57 +126,72 @@ export default function Ticket({ params }: { params: { id: string } }) {
   return (
     <div className="min-w-screen relative flex min-h-full flex-col items-center justify-center gap-2 ">
       {/* the ticket */}
-      {ticket && (
-        <div className="mb-7 flex rotate-90  scale-75 bg-pink-600 shadow-lg shadow-dark-300 xs:scale-90 sm:rotate-0 sm:scale-100">
-          <div className=" flex">
-            <div className="ml-7 h-full w-[300px] bg-opacity-80 bg-[url('https://cdn-az.allevents.in/events1/banners/039e456619af4a1c7d596d1cc7b29bb0d125a6cecaa51fcdfb46f79e4aad958c-rimg-w768-h960-dc162035-gmir?v=1713581427')] bg-cover font-liches font-bold sm:w-[300px] ">
-              <p className="absolute -ml-6 flex h-full -rotate-180 justify-around py-3 text-center text-base tracking-[0.15em] text-light-600 [writing-mode:vertical-rl] ">
+      {ticket && event && (
+        <div className="mb-7 flex rotate-90  scale-75 bg-brand shadow-lg shadow-dark-300 xs:scale-90 sm:rotate-0 sm:scale-100">
+          <div className="flex">
+            <div className="relative ml-7 h-[300px] w-[300px] bg-opacity-80 font-liches font-bold">
+              <p className="z-10 -ml-6 flex h-full -rotate-180 justify-around py-3 text-center text-base tracking-[0.15em] text-light-600 [writing-mode:vertical-rl] ">
                 <span className="opacity-80">ADMIT ONE</span>
                 <span className="!text-light-300">ADMIT ONE</span>{" "}
                 <span className="opacity-80">ADMIT ONE</span>
               </p>
-              <div className="flex h-full w-full items-end justify-end  p-1.5 text-xl  tracking-wide text-white  ">
+              <Image
+                src={event.cover}
+                alt={event.name}
+                fill
+                className=" object-cover"
+              />
+
+              {/* <div className="flex items-end justify-end  p-1.5 text-xl  tracking-wide text-white  ">
                 <p className=" ">#{ticket.ticket_number}</p>
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="flex min-w-[500px] flex-col justify-between justify-items-stretch bg-light-300 px-10 py-3 text-center ">
-            <p className="flex items-center justify-around border-y border-dark-200 py-1.5 font-liches font-medium text-dark-300 ">
-              <span className="w-[100px] text-left text-lg ">FRI - SUN</span>
-              <span className="w-[100px] text-[20px] font-semibold text-pink-500  ">
-                APR 26 - 28
+            <p className="flex items-center justify-around border-b border-dark-200 pb-2 font-liches font-medium text-dark-300 ">
+              {/* <span className="w-[100px] text-left text-lg ">
+                {startDate?.day === endDate?.day
+                  ? startDate?.dayOfWeek
+                  : `${startDate?.dayOfWeek} - ${endDate?.dayOfWeek}`}
+              </span> */}
+              <span className="w-[100px] text-[20px] font-medium   ">
+                {startDate?.month === endDate?.month && startDate?.month}
+                {startDate?.month === endDate?.month &&
+                  (startDate?.day === endDate?.day
+                    ? startDate?.day
+                    : `${startDate?.day} - ${endDate?.day}`)}
+                {startDate?.month !== endDate?.month &&
+                  ` ${startDate?.month} ${startDate?.day} - ${endDate?.month} ${endDate?.day}`}
               </span>
-              <span className="w-[100px] text-right text-lg ">2024</span>
+              <span className="w-[100px] text-right text-lg ">
+                {startDate?.year === endDate?.year
+                  ? startDate?.year
+                  : `${startDate?.year} - ${endDate?.year}`}
+              </span>
             </p>
             {/* event name */}
-            <div className=" font-nanum text-dark-200 ">
-              <h1 className="text-6xl font-semibold tracking-widest text-indigo-700 ">
-                {" "}
-                Malkia Summit{" "}
-              </h1>
-              <h2 className="text-5xl  text-pink-500 ">
+            <div className=" font-liches text-dark-400 ">
+              <h1 className=" mt-2 text-3xl tracking-widest">{event.name}</h1>
+              <h2 className="py-3 font-nanum text-5xl text-brand ">
                 {ticket.holders_name}
               </h2>
             </div>
-            <div className=" py-3 font-liches text-xl font-medium tracking-widest text-indigo-700 ">
+            <div className=" font-liches text-xl font-medium tracking-widest text-dark-300 ">
               <p>
-                9:00 AM <span>TO</span> 5:00 PM
-              </p>
-              <p>
-                Friday <span>TO</span> Sunday
+                {startTime ?? ""} <span>TO</span> {endTime ?? ""}
               </p>
             </div>
             <p className="flex w-full items-center justify-around border-t border-dark-200 pt-2 font-liches text-[20px] text-dark-200 ">
-              <span>Lifepool Chapel</span>
+              <span>{event.host}</span>
               <span className="separator">
                 <SmileyIcon className="h-7 w-7 stroke-2 " />
               </span>
-              <span>NOONKOPIR, Kitengela</span>
+              <span>{event.venue}</span>
             </p>
           </div>
 
           {/* right */}
-          <div className="relative flex items-center justify-center bg-pink-600 p-4 ">
+          <div className="relative flex items-center justify-center bg-brand p-4 ">
             <p className="absolute right-0 flex h-full -rotate-180 justify-around py-3 text-center font-liches text-base text-lg tracking-[0.15em] text-light-200 [writing-mode:vertical-lr] ">
               #{ticket.ticket_number}
             </p>
